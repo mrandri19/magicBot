8c46f05e242dd7f7fe5ba871e3ea2ac7edc2ee1a
mrandri19 <andrecogno@hotmail.it>
2015-10-19 21:43:18 +0200

    add folders

---
+++ test/test_crawler.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+sys.path.insert(0, "../src/crawler")
+import crawler
+class TestCrawler(unittest.TestCase):
+    def test_parse_request(self):
+        return True
+
+
+if __name__ == '__main__':
+    unittest.main()
